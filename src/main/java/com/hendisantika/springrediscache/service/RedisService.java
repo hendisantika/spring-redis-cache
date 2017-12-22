5109c1e7c4c8ddd087d4eb0019ca3246d3077c73
Hendi Santika <hendisantika@gmail.com>
2017-12-23 06:08:00 +0700

    #6 - Add Redis Service

---
+++ src/main/java/com/hendisantika/springrediscache/service/RedisService.java
@@ -0,0 +1,46 @@
+package com.hendisantika.springrediscache.service;
+
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-redis-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/12/17
+ * Time: 06.04
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service("cacheService")
+public class RedisService implements CacheService {
+    @Resource(name = "redisTemplate")
+    private ListOperations<String, String> messageList;
+
+    @Resource(name = "redisTemplate")
+    private RedisOperations<String, String> latestMessageExpiration;
+
+    @Override
+    public void addMessage(String user, String message) {
+
+        messageList.leftPush(user, message);
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        Date date = Date.from(zonedDateTime.plus(1, ChronoUnit.MINUTES).toInstant());
+        latestMessageExpiration.expireAt(user, date);
+    }
+
+    @Override
+    public List<String> listMessages(String user) {
+        return messageList.range(user, 0, -1);
+    }
+}
