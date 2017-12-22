1537f646f26cd9e749d619565339ffa93bf8fb92
Hendi Santika <hendisantika@gmail.com>
2017-12-23 05:59:46 +0700

    #1 - Intial Commit

---
+++ src/main/java/com/hendisantika/springrediscache/SpringRedisCacheApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springrediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisCacheApplication.class, args);
+    }
+}
